b219e9a9499a13a37e9f61da85509bbd42268859
Hajar <hajarelfadil2001@gmail.com>
2024-03-01 20:59:01 +0100

    update

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,19 +1,30 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_uncle - function that finds the uncle of a node
-* @node: pointer to the node to find the uncle
-* Return:  pointer to uncle node or NULL
-*/
+ * binary_tree_uncle - function that finds if a node is sibling
+ * @node: node to check
+ * Return: The uncle node
+ */
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
-
 {
-        if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
-        {
-                return (NULL);
-        }
-        if (node->parent->parent->left == node->parent)
-                return (node->parent->parent->right);
-        return (node->parent->parent->left);
+	binary_tree_t *_parent;
+
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	_parent = node->parent;
+
+	if (_parent->parent == NULL)
+		return (NULL);
+
+	if (_parent->parent->left != NULL && _parent->parent->right != NULL)
+	{
+		if (_parent->parent->left == _parent)
+			return (_parent->parent->right);
+
+		if (_parent->parent->right == _parent)
+			return (_parent->parent->left);
+	}
+	return (NULL);
 }
---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_right - add a node in the right of the parent
+ * if it exists it move down one level and add the new node first
+ * @parent: parent of the specified node
+ * @value: value of the node
+ * Return: NULL if it fails or the new node
+ */
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newNode;
+
+	if (!parent)
+		return (NULL);
+
+	newNode = malloc(sizeof(binary_tree_t));
+
+	if (!newNode)
+		return (NULL);
+
+	newNode->parent = parent;
+	newNode->n = value;
+	newNode->left = NULL;
+	newNode->right = parent->right;
+
+	if (parent->right)
+		parent->right->parent = newNode;
+	parent->right = newNode;
+
+	return (newNode);
+}
