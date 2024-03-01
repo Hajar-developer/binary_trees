20921c39e6af8aea1aa17ea6af2b03f63415c22d
Hajar <hajarelfadil2001@gmail.com>
2024-03-01 20:45:14 +0100

    binary trees

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -5,6 +5,7 @@
 * @node: pointer to the node to find the uncle
 * Return:  pointer to uncle node or NULL
 */
+
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
         if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
